4de5f5953637d85ac4aa8a3f3ecb7691bb5579f2
meowmeow2301 <145837487+meowmeow2301@users.noreply.github.com>
2024-05-09 22:24:13 +0700

    add admin view

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -1,8 +1,48 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
 public class AdminController {
 
+    @FXML
+    private Button banButton;
+
+    @FXML
+    private VBox chatServerBox;
+
+    @FXML
+    private VBox listThreadBox;
+
+    @FXML
+    private Button noticeButton;
+
+    @FXML
+    private TextField noticeText;
+
+    @FXML
+    private MenuButton setReasons;
+
+    @FXML
+    private Button showRoomButton;
+
+    @FXML
+    private Button showThreadsButton;
+
+    @FXML
+    private TextField takeId;
+
+    @FXML
+    private Button unbanButton;
+
+    @FXML
+    private Button warnButton;
     public void addMessage(String message) {
 //
+
     }
 }
+
